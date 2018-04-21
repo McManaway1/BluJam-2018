53c06c08606c12ab684714867dc87c92e62a2d51
Andrew McManaway <andrew.mcmanaway@gmail.com>
2018-04-21 14:52:42 +1200

    Object Management

--- game.js
+++ game.js
@@ -3,14 +3,15 @@ var player;
 var platforms = [];
 
 function init() {
-    this.arena = new arena();
-    this.player = new player("Sid", 200, 0, 10, 20);
+    this.arena = new Arena(400, 400);
+    this.player = new Player("Sid", 200, (400 - 20), 10, 20);
 
     this.arena.start();
 }
 
-function doTick() {
+function onTick() {
     arena.frame++;
+    player.draw();
 }
 
 init();
--- gameObj.js
+++ gameObj.js
@@ -1,22 +1,30 @@
-function player(name, x, y, width, height) {
+function Player(name, x, y, width, height) {
     this.name = name;
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+
+    this.draw = function () {
+        g2 = arena.context;
+        g2.fillRect(x, y, width, height);
+    }
 }
 
-function arena() {
+function Arena(width, height) {
+    this.width = width;
+    this.height = height;
+
     this.canvas = document.createElement("canvas");
 
     this.start =  function() {
-        this.canvas.width = 400;
-        this.canvas.height = 400;
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 
         this.frame = 0;
-        this.interval = setInterval(doTick, 20);
+        this.interval = setInterval(onTick, 20);
     }
 
     this.clear = function() {
